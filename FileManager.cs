e4f91b58c1cd6192d89ed4b21e438b19135e822b
Mark McArthey <mmcarthey@wctc.edu>
2023-08-31 21:25:42 -0500

    Initial create

---
+++ FileManager.cs
@@ -0,0 +1,41 @@
+﻿namespace Class02File
+{
+    public class FileManager
+    {
+        public void Write(string filename)
+        {
+            StreamWriter sw = new StreamWriter(filename, true);
+
+            Console.WriteLine("Enter a course");
+            var course = Console.ReadLine();
+
+            Console.WriteLine("Enter a grade");
+            var grade = Console.ReadLine();
+
+            sw.WriteLine($"{course}|{grade}");
+
+            sw.Close();
+
+        }
+
+        public void Read(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                StreamReader sr = new StreamReader(filename);
+
+                sr.ReadLine();
+                while (sr.EndOfStream != true)
+                {
+                    var line = sr.ReadLine();
+                    Console.WriteLine($"Your line is {line}");
+
+                    // parse the line to retrieve the grade
+                    var lineArray = line.Split('|');
+
+                    Console.WriteLine($"Course: {lineArray[0]}, Grade: {lineArray[1]}");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace Class02File
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FileManager dataManager = new FileManager();
+           
+            string filename = "grades.txt";
+
+            // menu
+            string menu;
+            do
+            {
+                Console.WriteLine("1. Write data to file");
+                Console.WriteLine("2. Read data from file");
+                Console.WriteLine("3. Exit");
+                menu = Console.ReadLine();
+
+                if (menu == "1")
+                {
+                    dataManager.Write(filename);
+                }
+                else if (menu == "2")
+                {
+                    dataManager.Read(filename);
+                }
+
+            } while (menu != "3");
+        }
+    }
+}
